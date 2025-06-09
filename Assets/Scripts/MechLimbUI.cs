23b0bfa8e1a97b267b240d181e85cc450185472b
dg155 <danielboghoss@gmail.com>
2025-06-09 10:40:01 -0700

    Got drag working, still fixing up drop

--- Assets/Scripts/MechLimbUI.cs
+++ Assets/Scripts/MechLimbUI.cs
@@ -4,119 +4,162 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class MechLimbUI : MonoBehaviour
+public class MechLimbUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+
+    [HideInInspector] public Transform parentAfterDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentAfterDrag = _image.transform.parent;
+        _image.transform.SetParent(_image.transform.root);
+        _image.transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _image.transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _image.transform.SetParent(parentAfterDrag);
+        _image.transform.SetAsFirstSibling();
+    }
+
     [SerializeField] Image _image;
     [SerializeField] TextMeshProUGUI _name;
     [SerializeField] TextMeshProUGUI _description;
 
     public UnityEvent SelectLimb; // Sends out signals that the current limb is selected
 
-    private ChassisType CurrentChassis;
-    private LimbType CurrentLimb;
+    /** Using Combined Parts Serialized Objects **/
+    [HideInInspector] public MechPart CurrentPart;
+    [SerializeField] private MechPart[] _exampleMechParts;
 
-    [SerializeField] ChassisType _example1; //delete this later
-    [SerializeField] ChassisType _example2; //delete this later
-    [SerializeField] ChassisType _example3; //delete this later
-    [SerializeField] LimbType _example4; //delete this later
-    [SerializeField] LimbType _example5; //delete this later
-    [SerializeField] LimbType _example6; //delete this later
-    [SerializeField] LimbType _example7; //delete this later
+    /** Using Seperated Parts Serialized Objects **/
+
+    // private ChassisType CurrentChassis;
+    // private LimbType CurrentLimb;
+
+    // [SerializeField] ChassisType _example1; //delete this later
+    // [SerializeField] ChassisType _example2; //delete this later
+    // [SerializeField] ChassisType _example3; //delete this later
+    // [SerializeField] LimbType _example4; //delete this later
+    // [SerializeField] LimbType _example5; //delete this later
+    // [SerializeField] LimbType _example6; //delete this later
+    // [SerializeField] LimbType _example7; //delete this later
 
     private void Start()
     {
-        var chassisList = new List<ChassisType> { _example1, _example2, _example3 };
-        var limbList = new List<LimbType> { _example4, _example5, _example6, _example7 };
 
-        bool chooseChassis = Random.value < 0.5f;
+        PopulateUI(_exampleMechParts[Random.Range(0, _exampleMechParts.Length)]);
 
-        if (chooseChassis)
-        {
-            ChassisType randomChassis = chassisList[Random.Range(0, chassisList.Count)];
-            PopulateUI(randomChassis);
-        }
-        else
-        {
-            LimbType randomLimb = limbList[Random.Range(0, limbList.Count)];
-            PopulateUI(randomLimb);
-        }
+        // var chassisList = new List<ChassisType> { _example1, _example2, _example3 };
+        // var limbList = new List<LimbType> { _example4, _example5, _example6, _example7 };
+
+        // bool chooseChassis = Random.value < 0.5f;
+
+        // if (chooseChassis)
+        // {
+        //     ChassisType randomChassis = chassisList[Random.Range(0, chassisList.Count)];
+        //     PopulateUI(randomChassis);
+        // }
+        // else
+        // {
+        //     LimbType randomLimb = limbList[Random.Range(0, limbList.Count)];
+        //     PopulateUI(randomLimb);
+        // }
     }
 
-    public void PopulateUI(LimbType limbType)
+    public void PopulateUI(MechPart mechPart)
     {
-        switch (limbType.LimbIndex)
-        {
-            case 0: // left arm
-                _image.sprite = limbType.leftArmData.leftArmSprite;
-                _name.text = limbType.leftArmData.leftArmName;
-                _description.text = limbType.leftArmData.leftArmDescription;
-                CurrentLimb = limbType;
-                break;
-            case 1: // right arm
-                _image.sprite = limbType.rightArmData.rightArmSprite;
-                _name.text = limbType.rightArmData.rightArmName;
-                _description.text = limbType.rightArmData.rightArmDescription;
-                CurrentLimb = limbType;
-                break;
-            case 2: // legs
-                _image.sprite = limbType.legsData.legsSprite;
-                _name.text = limbType.legsData.legsName;
-                _description.text = limbType.legsData.legsDescription;
-                CurrentLimb = limbType;
-                break;
-            default:
-                Debug.Log("No limb type assigned for: " + limbType);
-                break;
-        }
+        CurrentPart = mechPart;
+        _image.sprite = mechPart.partSprite;
+        _name.text = mechPart.partName;
+        _description.text = mechPart.partDescription;
     }
 
-    public void PopulateUI(ChassisType chassisType)
-    {
-        _image.sprite = chassisType.chassisSprite;
-        _name.text = chassisType.chassisName;
-        _description.text = chassisType.chassisDescription;
-        CurrentChassis = chassisType;
-    }
+    // public void PopulateUI(LimbType limbType)
+    // {
+    //     switch (limbType.LimbIndex)
+    //     {
+    //         case 0: // left arm
+    //             _image.sprite = limbType.leftArmData.leftArmSprite;
+    //             _name.text = limbType.leftArmData.leftArmName;
+    //             _description.text = limbType.leftArmData.leftArmDescription;
+    //             CurrentLimb = limbType;
+    //             break;
+    //         case 1: // right arm
+    //             _image.sprite = limbType.rightArmData.rightArmSprite;
+    //             _name.text = limbType.rightArmData.rightArmName;
+    //             _description.text = limbType.rightArmData.rightArmDescription;
+    //             CurrentLimb = limbType;
+    //             break;
+    //         case 2: // legs
+    //             _image.sprite = limbType.legsData.legsSprite;
+    //             _name.text = limbType.legsData.legsName;
+    //             _description.text = limbType.legsData.legsDescription;
+    //             CurrentLimb = limbType;
+    //             break;
+    //         default:
+    //             Debug.Log("No limb type assigned for: " + limbType);
+    //             break;
+    //     }
+    // }
+
+    // public void PopulateUI(ChassisType chassisType)
+    // {
+    //     _image.sprite = chassisType.chassisSprite;
+    //     _name.text = chassisType.chassisName;
+    //     _description.text = chassisType.chassisDescription;
+    //     CurrentChassis = chassisType;
+    // }
 
     public void SendSignal()
     {
         SelectLimb.Invoke();
-        if (CurrentChassis)
-        {
-            //Set chassis display
-            GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart(0, CurrentChassis.chassisSprite);
-        }
-        else
-        {
-            // set a limb display
-            switch (CurrentLimb.LimbIndex)
-            {
-                case 0: // left arm
-                    GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart(CurrentLimb.LimbIndex + 1, CurrentLimb.leftArmData.leftArmSprite);
-                    break;
-                case 1: // right arm
-                    GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart(CurrentLimb.LimbIndex + 1, CurrentLimb.rightArmData.rightArmSprite);
-                    break;
-                case 2: // legs
-                    GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart(CurrentLimb.LimbIndex + 1, CurrentLimb.legsData.legsSprite);
-                    break;
-                default:
-                    Debug.Log("No limb type assigned for: " + CurrentLimb);
-                    break;
-            }
-        }
+
+        GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart((int)CurrentPart.mechPartType, CurrentPart.partSprite);
+
+        // if (CurrentChassis)
+        // {
+        //     //Set chassis display
+        //     GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart(0, CurrentChassis.chassisSprite);
+        // }
+        // else
+        // {
+        //     // set a limb display
+        //     switch (CurrentLimb.LimbIndex)
+        //     {
+        //         case 0: // left arm
+        //             GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart(CurrentLimb.LimbIndex + 1, CurrentLimb.leftArmData.leftArmSprite);
+        //             break;
+        //         case 1: // right arm
+        //             GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart(CurrentLimb.LimbIndex + 1, CurrentLimb.rightArmData.rightArmSprite);
+        //             break;
+        //         case 2: // legs
+        //             GameObject.FindGameObjectWithTag("BuildABotScreen").GetComponent<BuildABotScreen>().SetNewMechPart(CurrentLimb.LimbIndex + 1, CurrentLimb.legsData.legsSprite);
+        //             break;
+        //         default:
+        //             Debug.Log("No limb type assigned for: " + CurrentLimb);
+        //             break;
+        //     }
+        // }
     }
 
     public void selectedLimb()
     {
-        if (CurrentChassis)
-        {
-            Debug.Log("Selected Limb:" + CurrentChassis.chassisName);
-        }
-        else
-        {
-            Debug.Log("Selected Limb:" + CurrentLimb);
-        }
+        Debug.Log("Selected Limb:" + CurrentPart.partName);
+
+        // if (CurrentChassis)
+        // {
+        //     Debug.Log("Selected Limb:" + CurrentChassis.chassisName);
+        // }
+        // else
+        // {
+        //     Debug.Log("Selected Limb:" + CurrentLimb);
+        // }
     }
 }
---
+++ Assets/Scripts/MechPart.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MechPartType
+{
+    Chassis,
+    Arm,
+    Legs
+}
+
+[CreateAssetMenu(fileName = "MyMechPart", menuName = "ScriptableObjects/MechPart", order = 3)]
+public class MechPart : ScriptableObject
+{
+    // Fields that can be configured without compiling.
+    public MechPartType mechPartType;
+    public int maxHealth;
+    public int damage;
+    public int speed;
+    public Sprite partSprite;
+    public string partName;
+    public string partDescription;
+
+    // // Fields can also be Unity stuff.
+    // public GameObject modelPrefab;
+    // public Sprite sprite;
+
+    // // If we need additional behavior/hooks, we can split that off into another object like this.
+    // public Unstable.ChassisBehavior specificBehavior;
+
+    // // Or maybe an enum would be better? Bleh, I'll figure it out later.
+}
