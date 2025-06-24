b102f7ca64c47c416d92704e4a208897fda9d67a
Ryan <artlessavian@gmail.com>
2025-06-23 23:21:43 -0700

    raycast from camera to set target

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public float lifetime = 0;
     public Vector3 direction;
 
     void FixedUpdate()
     {
-        transform.position += direction.normalized * 5f * Time.fixedDeltaTime;
+        transform.position += direction.normalized * 20f * Time.fixedDeltaTime;
+        lifetime += Time.fixedDeltaTime;
+
+
+        if (lifetime > 10)
+        {
+            // a projectile probably doesn't live for more than 10s, right?
+            Destroy(this.gameObject);
+        }
     }
 }
