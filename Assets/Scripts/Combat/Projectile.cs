63c17110643dc4cdea9779ee3b86fe1c17a9c3a8
Ryan <artlessavian@gmail.com>
2025-06-24 00:06:00 -0700

    fully auto

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,22 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public ShootsProjectiles.Instance originator;
     public float lifetime = 0;
-    public Vector3 direction;
+    public Ray ray;
+
+    public int pierce = 0;
+    List<Collision> pierced = new List<Collision>();
+
+    public void FollowRay(Ray actualRay)
+    {
+        ray = actualRay;
+
+        transform.position = actualRay.origin;
+        transform.LookAt(ray.origin + ray.direction);
+    }
 
     void FixedUpdate()
     {
-        transform.position += direction.normalized * 20f * Time.fixedDeltaTime;
+        transform.position += ray.direction.normalized * 20f * Time.fixedDeltaTime;
         lifetime += Time.fixedDeltaTime;
-
 
         if (lifetime > 10)
         {
             // a projectile probably doesn't live for more than 10s, right?
             Destroy(this.gameObject);
+        }
+    }
+
+    // TODO: Currently never called?
+    void OnCollisionEnter(Collision collision)
+    {
+        originator.OnProjectileEnter(this, collision);
+
+        if (pierced.Count >= pierce)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            pierced.Add(collision);
         }
     }
 }
--- Assets/Scripts/Data/ArmBehavior/ProjectileArmBehavior.cs
+++ Assets/Scripts/Data/ArmBehavior/ProjectileArmBehavior.cs
@@ -1,10 +1,15 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [CreateAssetMenu(menuName = "ArmBehavior/ShootsProjectiles")]
 public class ShootsProjectiles : ArmBehaviorData
 {
     public GameObject projectilePrefab;
+    public float rpm = 900;
+    public int magSize = 20;
+    public float reloadPeriodSeconds = 1;
+    public int reloadIncrement = 20;
 
     public override IArmBehavior MakeInstance()
     {
@@ -15,47 +20,74 @@ public class ShootsProjectiles : ArmBehaviorData
     public class Instance : IArmBehavior
     {
         public ShootsProjectiles data;
+        public bool active;
+        public float shotCooldown; // counts down to 0
+        public int magCount;
+        public float reloadCooldown; // counts down to 0
 
         public void Activate(GameObject owner, ArmInstance arm)
+        {
+            active = true;
+        }
+
+        public void Deactivate(GameObject owner, ArmInstance arm)
+        {
+            active = false;
+        }
+
+        public void FixedUpdate(GameObject owner, ArmInstance arm)
+        {
+            if (shotCooldown > 0)
+            {
+                shotCooldown -= Time.fixedDeltaTime;
+            }
+            if (reloadCooldown > 0)
+            {
+                reloadCooldown -= Time.fixedDeltaTime;
+            }
+
+            if (active)
+            {
+                reloadCooldown = data.reloadPeriodSeconds;
+                if (shotCooldown <= 0 && magCount > 0)
+                {
+                    shotCooldown += 60 / data.rpm;
+                    magCount -= 1;
+                    Shoot(owner.transform.position + Vector3.up * 1.5f);
+                }
+            }
+            else
+            {
+                if (reloadCooldown <= 0 && magCount < data.magSize)
+                {
+                    reloadCooldown += data.reloadPeriodSeconds;
+                    magCount += data.reloadIncrement;
+                    magCount = Math.Min(magCount, data.magSize);
+                }
+            }
+        }
+
+        internal void OnProjectileEnter(Projectile projectile, Collision collision)
+        {
+            Debug.Log(collision);
+        }
+
+        void Shoot(Vector3 origin)
         {
             Ray cameraRay = new Ray(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward));
 
             RaycastHit hitInfo;
             bool hit = Physics.Raycast(cameraRay, out hitInfo, 10);
+            Vector3 target = hit ? hitInfo.point : cameraRay.origin + cameraRay.direction.normalized * 10;
+
+            Ray actualRay = new Ray();
+            actualRay.origin = origin;
+            actualRay.direction = target - actualRay.origin;
 
             var instance = Instantiate(data.projectilePrefab);
             var projectile = instance.GetComponent<Projectile>();
-
-            Vector3 instance_position = owner.transform.position + Vector3.up * 1.5f;
-            instance.transform.position = instance_position;
-
-            Vector3 target;
-            if (hit)
-            {
-                target = hitInfo.point;
-            }
-            else
-            {
-                target = cameraRay.origin + cameraRay.direction.normalized * 10;
-            }
-
-            Debug.Log(hit);
-            Debug.Log(target);
-
-            Vector3 direction = target - instance_position;
-
-            instance.transform.LookAt(target);
-            projectile.direction = direction;
-        }
-
-        public void Deactivate(GameObject owner, ArmInstance arm)
-        {
-
-        }
-
-        public void FixedUpdate(GameObject owner, ArmInstance arm)
-        {
-
+            projectile.FollowRay(actualRay);
+            projectile.originator = this;
         }
     }
 }
