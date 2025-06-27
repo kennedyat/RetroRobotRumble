12fce198db1be8a85fe4f5aa6710275e697b1767
kennedyat <ktpenny8@gmail.com>
2025-06-27 13:48:50 -0700

    Refactor Lunge

--- Assets/Scripts/Data/Abilities/Lunge.cs
+++ Assets/Scripts/Data/Abilities/Lunge.cs
@@ -1,45 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Abilities/Lunge")]
-public class Lunge : Abilities
-{
-    // Start is called before the first frame update
 
+[CreateAssetMenu(menuName = "ArmBehavior/Lunge")]
+public class Lunge : ArmBehaviorData
+{
     [Header("Lunge Parameters")]
     public float distance = 5f;
     public float speed = 10f;
+    public float duration = 0.3f;
+    public float cooldown = 1.2f;
 
-
-
-    public override void Effect(GameObject effected, GameObject player)
+    public override IArmBehavior MakeInstance()
     {
+        return new Instance { data = this };
+    }
+}
+    [Serializable]
+    public class Instance : IArmBehavior
+    {
+        public Lunge data;
+        public bool active;
+        public float actionCooldown;
+        public float actionDuration;
 
-        if (effected.TryGetComponent<Rigidbody>(out var rb))
+    public void Activate(GameObject owner, ArmInstance arm)
+    {
+        if (active|| actionCooldown>0) return;
+
+        active = true;
+            
+        actionCooldown = data.cooldown;
+        actionDuration = data.duration;
+    }
+
+    public void Deactivate(GameObject owner, ArmInstance arm)
+    {
+            active = false;
+    }
+
+   
+    public void FixedUpdateFromArm(GameObject owner, ArmInstance arm)
+    {
+        actionCooldown = Mathf.Max(0, actionCooldown - Time.fixedDeltaTime);
+
+        actionDuration = Mathf.Max(0, actionDuration - Time.fixedDeltaTime);
+
+        if (owner.gameObject.TryGetComponent<Rigidbody>(out var rb))
         {
-            if (!onHit)
+            if (actionDuration > 0 && actionCooldown > 0)
             {
-                Vector3 pos = Vector3.Lerp(rb.position, rb.position + (effected.transform.forward * speed), Time.fixedDeltaTime);
+                Vector3 pos = Vector3.Lerp(rb.position, rb.position + (owner.transform.forward * data.speed), Time.fixedDeltaTime);
                 rb.MovePosition(pos);
 
-                Debug.Log("Lunging");
             }
-            else
-            {
-                Debug.Log("Lunge stopped");
-            }
-
-
+        
         }
 
     }
 
-    public override void OnCollision(GameObject effected, GameObject player)
-    {
+   
         
-    }
-
 }
+
+
