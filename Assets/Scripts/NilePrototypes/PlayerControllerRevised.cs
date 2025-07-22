072771798c62aa1a91528a941995c3bf9e08e9d0
nimtiaz-usc <nile.imtiaz01@gmail.com>
2025-07-22 02:03:56 -0700

    implemented updated aiming

---
+++ Assets/Scripts/NilePrototypes/PlayerControllerRevised.cs
@@ -0,0 +1,190 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerRevised : MonoBehaviour
+{
+    private Vector2 moveInput;
+    private Rigidbody rigidbody;
+    private CharacterController characterController;
+    private Vector3 moveDirection;
+
+    [SerializeField] private bool manualAim = true;
+    private Vector2 aimInput;
+    [SerializeField] private float smoothTime = 0.05f;
+    [SerializeField] private Transform cameraTarget;
+    private float currentVelocity;
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    [SerializeField] private float dashDistance = 5f;
+    [SerializeField] private float dashDuration = 0.5f;
+    private bool isDashing = false;
+
+    private float gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 3f;
+    private float verticalVelocity;
+
+    [SerializeField] private float jumpPower;
+    private int currentJumps;
+    [SerializeField] private int maxJumps = 2;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(aimInput);
+
+        if (!manualAim && moveInput.sqrMagnitude != 0)
+        {
+            ApplyRotation();
+        }
+        ApplyMovement();
+        ApplyGravity();
+
+        bool centerCamera = moveInput.sqrMagnitude == 0f && aimInput.sqrMagnitude == 0f;
+
+        if (centerCamera)
+        {
+            cameraTarget.DOLocalMoveZ(0, 1f);
+        } 
+        else
+        {
+            cameraTarget.DOLocalMoveZ(1, 1f);
+        }
+    }
+
+    private void ApplyRotation()
+    {
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+        float smoothedAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0f, smoothedAngle, 0f);
+    }
+
+    private void ApplyMovement()
+    {
+        characterController.Move(moveSpeed * Time.deltaTime * moveDirection);
+    }
+
+    private void ApplyGravity()
+    {
+        if (IsGrounded() && verticalVelocity < 0f)
+        {
+            verticalVelocity = -1f;
+        } else
+        {
+            verticalVelocity += gravity * gravityMultiplier * Time.deltaTime;
+        }
+        
+        moveDirection.y = verticalVelocity;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        moveDirection = new Vector3(moveInput.x, 0f, moveInput.y);            
+    }
+
+    public void Aim(InputAction.CallbackContext context)
+    {
+        aimInput = context.ReadValue<Vector2>();
+
+        if (context.control.device is Mouse)
+        {
+            MouseAim();
+        }
+        else if (context.control.device is Gamepad)
+        {
+            GamepadAim();
+        }
+    }
+
+    private void MouseAim()
+    {
+        manualAim = true;
+
+        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 raycastPoint = ray.GetPoint(rayDistance);
+            Vector3 lookPoint = new Vector3(raycastPoint.x, transform.position.y, raycastPoint.z);
+            transform.LookAt(lookPoint);
+        }
+    }
+
+    private void GamepadAim()
+    {
+        if (aimInput.sqrMagnitude == 0f)
+        {
+            manualAim = false;
+            return;
+        } 
+        else
+        {
+            manualAim = true;
+
+            Vector3 inputDirection = Vector3.right * aimInput.x + Vector3.forward * aimInput.y;
+            if (inputDirection.sqrMagnitude > 0f)
+            {
+                float targetRotationY = Mathf.Atan2(aimInput.x, aimInput.y) * Mathf.Rad2Deg;
+                Quaternion targetRotation = Quaternion.Euler(0f, targetRotationY, 0f);
+                transform.rotation = targetRotation;
+            }
+        }
+
+    }
+
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (context.started && !isDashing && moveInput.sqrMagnitude != 0)
+        {
+            isDashing = true;
+            StartCoroutine(DashMovement());
+        }
+    }
+
+    private IEnumerator DashMovement()
+    {
+        transform.DOMoveX(transform.position.x + (moveDirection.x * dashDistance), dashDuration).SetEase(Ease.OutSine);
+        transform.DOMoveZ(transform.position.z + (moveDirection.z * dashDistance), dashDuration).SetEase(Ease.OutSine);
+        yield return new WaitForSeconds(dashDuration);
+
+        isDashing = false;
+        yield return null;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.started || (!IsGrounded() && currentJumps >= maxJumps))
+        {
+            return;
+        }
+
+        if (currentJumps == 0)
+        {
+            StartCoroutine(WaitForLanding());
+        }
+
+        currentJumps++;
+        verticalVelocity = jumpPower;
+    }
+
+    private IEnumerator WaitForLanding()
+    {
+        yield return new WaitUntil(() => !IsGrounded());
+        yield return new WaitUntil(IsGrounded);
+        currentJumps = 0;
+    }
+
+    private bool IsGrounded() => characterController.isGrounded;
+}
