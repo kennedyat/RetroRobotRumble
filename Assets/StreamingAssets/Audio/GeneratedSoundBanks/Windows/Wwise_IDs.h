8c2d2d382ad6152a4c457b6b4bd5f8dbb9790bcf
tononwiman <tan.onwimon@gmail.com>
2025-09-07 14:38:47 -0700

    Add Music Manager Script into Unity

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Windows/Wwise_IDs.h
@@ -0,0 +1,57 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID WW_PLAY_BATTLE1_LOOP = 1392930959U;
+        static const AkUniqueID WW_PLAY_BUILDABOT_LOOP = 3222909602U;
+        static const AkUniqueID WW_PLAY_SFX_TEST = 2425946027U;
+        static const AkUniqueID WW_START_MUSIC = 2712634978U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace BATTLE
+        {
+            static const AkUniqueID GROUP = 2937832959U;
+
+            namespace STATE
+            {
+                static const AkUniqueID BATTLE_1 = 968711815U;
+                static const AkUniqueID BUILD_A_BOT = 3246825821U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace BATTLE
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID PROTOTYPE_TEST = 879528288U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
