400716e0b064786922dfbab0466ee91e2692bab5
nile-imtiaz <nile.imtiaz01@gmail.com>
2025-09-25 11:26:41 -0700

    standardized prefab mesh placements

---
+++ Assets/Scripts/BuildABot/BAB_SpawnParts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BAB_SpawnParts : MonoBehaviour
+{
+    [SerializeField] GameObject[] parts; //replace with something that reads from a data struct of all the player's unlocked parts
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
