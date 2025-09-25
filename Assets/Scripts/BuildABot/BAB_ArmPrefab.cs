3cf652150cd333a827e0b665500dac4249948e45
nile-imtiaz <nile.imtiaz01@gmail.com>
2025-09-24 18:21:23 -0700

    locomotive and shark arm prefabs

--- Assets/Scripts/BuildABot/BAB_ArmPrefab.cs
+++ Assets/Scripts/BuildABot/BAB_ArmPrefab.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 
 public class BAB_ArmPrefab : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Tooltip("The name of the arm")]
+    public string _armName = "Arm Name";
+
+    [Tooltip("The description of the arm")]
+    public string _armDescription = "Arm Description";
+    
+    [Tooltip("The name of the arm's basic attack")]
+    public string _basicName = "Basic Attack Name";
+
+    [Tooltip("The description of the arm's basic attack")]
+    public string _basicDescription = "Basic Attack Description";
+    
+    [Tooltip("The name of the arm's special attack")]
+    public string _specialName = "Special Attack Name";
+
+    [Tooltip("The description of the arm's special attack")]
+    public string _specialDescription = "Special Attack Description";
+
+    [Tooltip("All of the recolored materials available for this arm")]
+    public Material[] _armMaterials;
+
     void Start()
     {
         
