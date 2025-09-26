b2ab09a3dcd92cbe7862e5d2faf157c07d66d3e2
nile-imtiaz <nile.imtiaz01@gmail.com>
2025-09-26 13:52:53 -0700

    starting selection logic

---
+++ Assets/Scripts/BuildABot/BAB_SelectPart.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class BAB_SelectPart : MonoBehaviour
+{
+    private GameObject selectedPart;
+    private Rigidbody selectedRB;
+
+    [SerializeField, Tooltip("The height the selected part will snap to")] float _selectionHeight = 3f;
+    [SerializeField, Tooltip("The time taken for the part to orient its rotation after being selected")] float _orientSpeed = 0.1f;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // replace with proper input later
+        {
+            RaycastHit hit = CastRay();
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Part"))
+                {
+                    selectedPart = hit.collider.gameObject;
+                    selectedRB = selectedPart.GetComponent<Rigidbody>();
+                    if (selectedRB == null)
+                    {
+                        selectedPart = null;
+                        return;
+                    }
+                    selectedRB.DORotate(Vector3.zero, _orientSpeed);
+                    selectedRB.isKinematic = true;
+                }
+            }
+        }
+
+        if (selectedPart != null)
+        {
+            Debug.Log(selectedPart);
+
+            if (Input.GetMouseButtonUp(0)) // replace with proper input later
+            {
+                if (selectedRB != null)
+                {
+                    selectedRB.isKinematic = false;
+                }
+                selectedRB = null;
+                selectedPart = null;
+            }            
+        }
+    }
+
+    private RaycastHit CastRay()
+    {
+        Vector3 screenMousePosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
+
+        Vector3 screenMousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+
+        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+        RaycastHit hit;
+        Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit);
+
+        return hit;
+    }
+}
