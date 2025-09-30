00379feda14354f7faf57d056f5290220a76da43
real3586 <kevinphan565@gmail.com>
2025-09-30 13:12:53 -0700

    a very descriptive commit title
    changed the damage system to be projectiles (and melee probably) calling a damage function on the enemies
    so basically they tell enemies to damage themselves
    also returns the damage dealt in case we need that for lifesteal or damage counters

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -7,11 +7,28 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField, Tooltip("The health of this enemy.")]
     int health;
 
-    void OnTriggerEnter(Collider other)
+    public int DealDamage(int damageToDeal)
     {
-        if (other.CompareTag("Projectile"))
-            this.health--;
-        if (this.health <= 0)
+        // realDamage is the amount of damage actually dealt ignoring overkill damage
+        // ex. dealing 10 to a 5 hp enemy counts as 5 dmg)
+        // in some games it doesnt work like this (like TFT, i know someone reading this is here)
+        // but no idea how we want it
+        int realDamage = 0;
+        if (health < damageToDeal)
+        {
+            realDamage = health;
+        }
+        else
+        {
+            realDamage = damageToDeal;
+        }
+
+        health -= realDamage;
+        if (health <= 0)
             Destroy(this.gameObject);
+
+        // use the return value if we need access to how much damage it did
+        // like lifesteal calculations or damage trackers
+        return realDamage;
     }
 }
