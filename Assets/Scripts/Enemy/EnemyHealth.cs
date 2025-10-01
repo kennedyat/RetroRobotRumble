001e39891c81be31b763d0e7e836cef2d5ba2937
real3586 <kevinphan565@gmail.com>
2025-10-01 15:19:55 -0700

    THATS OVERKILL
    overkill damage counts as actual damage now
    it doesnt just take into account the actual damage dealt, but the value of the damage

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -9,19 +9,13 @@ public class EnemyHealth : MonoBehaviour
 
     public int DealDamage(int damageToDeal)
     {
-        // realDamage is the amount of damage actually dealt ignoring overkill damage
-        // ex. dealing 10 to a 5 hp enemy counts as 5 dmg)
-        // in some games it doesnt work like this (like TFT, i know someone reading this is here)
-        // but no idea how we want it
-        int realDamage = 0;
-        if (health < damageToDeal)
-        {
-            realDamage = health;
-        }
-        else
-        {
-            realDamage = damageToDeal;
-        }
+        int realDamage = damageToDeal;
+
+        // insert any damage more calculations here
+        // realDamage = damageToDeal * damageResist * damageMultiplier;
+        
+        // nile told me (kevin) dont subtract for overkill damage
+        // if player deals 10 to a 5 hp enemy count it as 10 not 5
 
         health -= realDamage;
         if (health <= 0)
