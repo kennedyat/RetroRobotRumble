306cb045c6848390662b613bc699867a28d6a1a5
nile-imtiaz <nile.imtiaz01@gmail.com>
2025-10-15 15:21:11 -0700

    replaced MainBuildABot

--- Assets/Scripts/RRRSceneManager.cs
+++ Assets/Scripts/RRRSceneManager.cs
@@ -19,7 +19,7 @@ public class RRRSceneManager
     public static void LoadBuildABot()
     {
         // TODO: Build A Bot isn't independent. We have to initialize it here.
-        var thing = SceneManager.LoadSceneAsync("BuildABot3D");
+        var thing = SceneManager.LoadSceneAsync("MainBuildABot");
 
         //thing.completed += (AsyncOperation obj) =>
         //{
