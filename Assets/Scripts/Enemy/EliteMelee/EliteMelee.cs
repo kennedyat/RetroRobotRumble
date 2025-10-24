c5701847107f4ed57c3faff539a9f6a922a18b68
real3586 <kevinphan565@gmail.com>
2025-10-23 22:21:48 -0700

    temporary smite spell (lol? reference?)
    the elite enemy now attacks
    well its a temp attack
    it spawns a block that falls above the player and uses distance do log a statement telling us if it would do damage
    states are in place and properly used here

--- Assets/Scripts/Enemy/EliteMelee/EliteMelee.cs
+++ Assets/Scripts/Enemy/EliteMelee/EliteMelee.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EliteMelee : MonoBehaviour
@@ -18,8 +19,15 @@ public class EliteMelee : MonoBehaviour
     [SerializeField, Tooltip("The range of the enemy, or the range it needs to be within before initiating attacks")]
     float attackDistance = 5f;
     [Header("Attacking")]
+    [SerializeField, Tooltip("Temporary smite projectile to visualize the melee enemy's attack")]
+    GameObject TEMP_Smite;
+    [SerializeField, Tooltip("The distance the player needs to be within to take dmaage")]
+    float TEMP_distanceThreshold = 1f;
+    [SerializeField, Tooltip("The delay that a melee attack initially has")]
     float attackWindup = 0.2f;
+    [SerializeField, Tooltip("The length of the attack animation")]
     float attackLength = 0.3f;
+    [SerializeField, Tooltip("The refractory period after an attack (can't attack)")]
     float attackRecovery = 0.5f;
     bool playerAttackSet = false;
 
@@ -45,14 +53,41 @@ public class EliteMelee : MonoBehaviour
         // otherwise continue chasing
         else
         {
-            state = EliteMeleeStates.Chasing;
-            ChasePlayer();
+            // don't chase if the attack coroutine is in progress
+            if (!playerAttackSet)
+            {
+                state = EliteMeleeStates.Chasing;
+                ChasePlayer();
+            }
         }
     }
 
     IEnumerator AttackPlayerSequence()
     {
-        yield return null;
+        // attacks have a delay specified in the inspector
+        // during this time, save the position of the player
+        Vector3 playerPos = player.position;
+        yield return new WaitForSeconds(attackWindup);
+
+        // then TEMPORARILY summon a block and smite the player
+        GameObject newBLock = Instantiate(TEMP_Smite, player.position + Vector3.up * 3, Quaternion.identity);
+
+        // and TEMPORARILY deal damage based on distance to the stored position
+        if (Vector3.Distance(playerPos, player.position) <= TEMP_distanceThreshold)
+        {
+            Debug.Log("You got hit nerd");
+        }
+
+        // wait for the animation to end
+        yield return new WaitForSeconds(attackLength);
+
+        // then destroy the TEMP block
+        Destroy(newBLock);
+
+        // and wait for the refractory period to end
+        yield return new WaitForSeconds(attackRecovery);
+
+        // reset the coroutine variable to true
         playerAttackSet = false;
     }
     
