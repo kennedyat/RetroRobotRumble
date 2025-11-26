570d7b5bd1d0241ff271e7ccb5b2451ae377bba1
real3586 <kevinphan565@gmail.com>
2025-11-26 11:08:45 -0800

    enemy spawner scaffold code
    now spawns enemies
    what else did you want me to do, write an enemy spawner that doesn't spawn stuff?
    next steps:
    1) make the enemies functional because right now they do not do anything
    2) spawn enemies around the map
    3) (last priority) refine the spawning points function

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,40 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rand = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // hard coded for now
-    int[] pCosts = {1, 2, 4};
-    [SerializeField] List<GameObject> enemyPrefabs;
+    [Serializable]
+    struct EnemySpawnData
+    {
+        public GameObject prefab;
+        public int cost;
+    }
+
+    [SerializeField] List<EnemySpawnData> enemyPrefabs;
     [SerializeField] int currentPoints;
+    [SerializeField] int startingPoints;
+    [SerializeField] int currentRound = 0;
 
     void Start()
     {
-        StartCoroutine(EnemySpawnSequence());
+        StartCoroutine(WaveSequence());
     }
 
     IEnumerator EnemySpawnSequence()
     {
-        // 1/3: get the available enemies to spawn
-        List<GameObject> canSpawn = new();
-        for (int i = 0; i < enemyPrefabs.Count; i++)
+        while (currentPoints > 0)
         {
-            if (pCosts[i] <= currentPoints)
+            // 1/3: get the available enemies to spawn
+            List<EnemySpawnData> canSpawn = new();
+            foreach (EnemySpawnData e in enemyPrefabs)
             {
-                canSpawn.Add(enemyPrefabs[i]);
+                if (e.cost <= currentPoints)
+                {
+                    canSpawn.Add(e);
+                }
             }
+
+            // 2/3: pick a random one
+            int random = Rand.Range(0, canSpawn.Count);
+
+            // 3/3: spawn it
+            GameObject reference = Instantiate(canSpawn[random].prefab, transform.position, Quaternion.identity);
+
+            // NOTE: the enemies will need some sort of initialization function because they dont have a reference to the player yet
+            // pathfinding and attacking will NOT work
+            //reference.initialize();
+
+            currentPoints -= canSpawn[random].cost;
+            Debug.Log("spawned " + canSpawn[random].prefab + " with " + currentPoints + " points left");
+
+            // wait some time, hard coded for now
+            yield return new WaitForSeconds(2.0f);
         }
-
-        // 2/3: pick a random one
-        int random = Random.Range(0, canSpawn.Count);
-
-        // 3/3: spawn it
-        //Instantiate(something something something);
-
-        // recursive for now
-        yield return new WaitForSeconds(1.0f);
-        if (currentPoints > 0) StartCoroutine(EnemySpawnSequence());
         yield return null;
+    }
+
+    IEnumerator WaveSequence()
+    {
+        // DEBUG: wait a few seconds before starting it
+        if (currentRound == 0) yield return new WaitForSeconds(5.0f);
+
+        // 1/2: set the number of points to double
+        // current round starts at zero, so increment it first
+        currentRound++;
+        startingPoints = (int)Mathf.Pow(2, currentRound);
+
+        // 2/2: pause execution of this coroutine and run the spawn sequence
+        currentPoints = startingPoints;
+        yield return StartCoroutine(EnemySpawnSequence());
+
+        // debugging stuff
+        yield return new WaitForSeconds(5.0f);
+
+        // recurse but stop if we are at round 5
+        if (currentRound < 5) StartCoroutine(WaveSequence());
     }
 }
