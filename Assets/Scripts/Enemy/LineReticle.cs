81d0241ee855553b2e48c91f915e7113c7f4cdae
real3586 <kevinphan565@gmail.com>
2026-01-31 12:01:38 -0800

    fix line reticle
    some weird math formula i was using and needed to change it

--- Assets/Scripts/Enemy/LineReticle.cs
+++ Assets/Scripts/Enemy/LineReticle.cs
@@ -14,7 +14,7 @@ public class LineReticle : MonoBehaviour
 
     public void Init(float l, float t, float w, bool raycastToWall = false)
     {
-        if (scaleFactorX == -1) 
+        if (scaleFactorX == -1)
         {
             scaleFactorX = transform.localScale.x / lrBase.transform.localScale.x;
             scaleFactorZ = transform.localScale.z / lrBase.transform.localScale.z;
@@ -32,6 +32,7 @@ public class LineReticle : MonoBehaviour
         // and move this a little bit forward or back depending on the range
         // this formula comes from desmos linear regression lol
         float forwardPos = 0.05f * length;
+        forwardPos = Mathf.Min(forwardPos, 0.75f);
         transform.localPosition = new(0, -1, forwardPos);
 
         // call the coroutine which makes the line go
@@ -43,14 +44,14 @@ public class LineReticle : MonoBehaviour
         float t = 0;
         while (t < time)
         {
-            if (raycastToWall) 
+            if (raycastToWall)
             {
                 // resize this depending on where the next wall in sight is
                 LayerMask mask = LayerMask.GetMask("Level");
                 Vector3 adjustedSpawnPos = transform.position + Vector3.up * 0.5f;
-                Physics.Raycast(adjustedSpawnPos, transform.forward, 
+                Physics.Raycast(adjustedSpawnPos, transform.forward,
                     out RaycastHit hit, 1000, mask, QueryTriggerInteraction.Collide);
-                
+
                 //Debug.DrawRay(adjustedSpawnPos, hit.point - adjustedSpawnPos, Color.red);
                 //Debug.DrawRay(transform.position, hit.point - transform.position, Color.green);
 
@@ -58,9 +59,9 @@ public class LineReticle : MonoBehaviour
                 length = hit.distance * 1.11f;
                 lrBase.transform.localScale = new(width / scaleFactorX, length / 2f / scaleFactorZ, 1f);
             }
-            
+
             lrExpander.transform.localScale = new(width / scaleFactorX, Mathf.Lerp(0, length / 2f / scaleFactorZ, t / time));
-            
+
             t += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
