4b456fbd0d52de33eae9abe02842ad6c78e17a3b
nimtiaz-usc <nile.imtiaz01@gmail.com>
2026-02-02 04:13:28 -0800

    moved sticker buff info to BAB

--- Assets/PopulateInfo.cs
+++ Assets/PopulateInfo.cs
@@ -11,16 +11,16 @@ public class PopulateInfo : MonoBehaviour
     {
         if (StickerBehavior.Instance != null)
         {
-            statsText.text = $"Tab to Victory Screen\n" +
-                            $"Attack Damage: {StickerBehavior.Instance.GetAttackDamage()}\n" +
-                           $"Crit Chance: {StickerBehavior.Instance.GetCritChance()}%\n" +
-                           $"Max Health: +{StickerBehavior.Instance.GetMaxHealthBonus()}\n" +
-                           $"Move Speed: {StickerBehavior.Instance.currentStickerMods.moveSpeed}\n" +
-                           $"Attack Speed: {StickerBehavior.Instance.currentStickerMods.attackSpeed}\n" +
-                           $"Special CD: {StickerBehavior.Instance.currentStickerMods.specialCooldown}\n" +
-                           $"Ult Charge: {StickerBehavior.Instance.currentStickerMods.ultimateCharge}\n" +
-                           $"Lifesteal: {StickerBehavior.Instance.currentStickerMods.lifesteal}\n" +
-                           $"Damage Res: {StickerBehavior.Instance.currentStickerMods.damageRes}";
+            statsText.text = //$"Tab to Victory Screen\n" +
+                            $"Attack Damage: + {StickerBehavior.Instance.GetAttackDamage()}%\n" +
+                           $"Crit Chance: + {StickerBehavior.Instance.GetCritChance()}%\n" +
+                           $"Max Health: + {StickerBehavior.Instance.GetMaxHealthBonus()}\n" +
+                           $"Move Speed: + {StickerBehavior.Instance.currentStickerMods.moveSpeed}%\n" +
+                           $"Attack Speed: + {StickerBehavior.Instance.currentStickerMods.attackSpeed}%\n" +
+                           $"Special CD: - {StickerBehavior.Instance.currentStickerMods.specialCooldown}%\n" +
+                           $"Ult Charge: - {StickerBehavior.Instance.currentStickerMods.ultimateCharge}%\n" +
+                           $"Lifesteal: + {StickerBehavior.Instance.currentStickerMods.lifesteal}%\n" +
+                           $"Damage Res: - {StickerBehavior.Instance.currentStickerMods.damageRes}%";
         }
         else
         {
