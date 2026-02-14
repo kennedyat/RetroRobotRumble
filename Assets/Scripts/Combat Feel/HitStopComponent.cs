d72c53b54c059932696aa1a22bddd327781e45cd
Wozzyb <shafikd@uci.edu>
2026-02-13 22:16:19 -0800

    started on component
    started work on universal hitstop component

---
+++ Assets/Scripts/Combat Feel/HitStopComponent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class HitStopCode : MonoBehaviour
+{
+    // sets up the Component
+    [Header("Hit Stop Connection")]
+    [SerializeField, Tooltip("Hitstop Manager")]
+    GameObject hitStopManager;
+
+
+
+}
--- Assets/Scripts/Combat Feel/HitStopManager.cs
+++ Assets/Scripts/Combat Feel/HitStopManager.cs
@@ -6,15 +6,19 @@ using UnityEngine;
 public class HitStopManager : MonoBehaviour
 {
 
-    // sets the default number of hitStops to 0
-    public enum HitStop { numberOfStops = 0 }
+    // sets up the manager
     [Header("Hit Stop Options")]
     [SerializeField, Tooltip("Hitstop flag.")]
     bool isHitStopActive = false;
-    [SerializeField, Tooltip("Hitstop script, it causes the actual hit stop")]
-    GameObject hitStopScript;
     [SerializeField, Tooltip("Where you check which type of hit stop you are applying.")]
     GameObject hitStopObject;
+
+
+    //Hitstop should be called once per activation! This keeps track of that
+    [Header("Combat Feel")]
+    [SerializeField] protected float GlobalHitstopTime = 0.02f;
+    [SerializeField] protected float DeathHitstopTime = 0.08f;
+    float uniqueHitStopTimer; //just to make sure we have a variable to store the unique (if we want) hit stop timers based on abilities or other triggers.
 
 
     // Sets isHitStop to false at the start incase somehow it got messed up during initialization 
@@ -26,9 +30,49 @@ public class HitStopManager : MonoBehaviour
     //returns isHitStop, True would mean there is HitStop Active, False would mean the game isn't using hitstop at the moment
     public bool isHitStopped()
     { return isHitStopActive; }
+
+    //StartCoroutine(nameof(GlobalHitstop));
+
+
+
+    public void hitStopinitiator(float uniqueHitStopTime)
+    {
+        if (isHitStopActive)
+        {
+
+        }
+        else
+        {
+            uniqueHitStopTimer = uniqueHitStopTime;
+            StartCoroutine(nameof(GlobalHitstop));
+
+        }
     }
 
-    //public void hitStopType()
-    //{
 
-    //}
+
+
+
+
+
+    public IEnumerator GlobalHitstop()
+    {
+        Time.timeScale = 0.0f;
+        isHitStopActive = true;
+        yield return new WaitForSecondsRealtime(GlobalHitstopTime);
+        Time.timeScale = 1.0f;
+        isHitStopActive = false;
+    }
+
+    public IEnumerator UniqueHitstop()
+    {
+        Time.timeScale = 0.0f;
+        isHitStopActive = true;
+        yield return new WaitForSecondsRealtime(uniqueHitStopTimer);
+        Time.timeScale = 1.0f;
+        isHitStopActive = false;
+    }
+
+
+}
+
